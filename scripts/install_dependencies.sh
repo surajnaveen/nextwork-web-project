ade54ba9db5918298c031651d1e15b4cd322fc8b
suraj <suraj0nb@gmail.com>
2026-05-07 10:46:43 +0000

    Change the install_dependancies.sh file, make a error in package install intentionally to check rollback function

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install tomcat -y
-sudo yum -y install httpd
+sudo yum -y install ht
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
