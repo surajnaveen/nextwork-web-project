553a9993aa68974fc487324349b042ffb988bcfe
suraj <suraj0nb@gmail.com>
2026-05-07 11:05:03 +0000

    Change the install_dependancies.sh file, fix the errors

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install tomcat -y
-sudo yum -y install 
+sudo yum -y install httpd
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
